05f449aed12ed5d4bde4fa4044b7dd9ac82ff2cc
Maria Olsen <69918976+mcolsen@users.noreply.github.com>
2021-05-27 13:51:37 -0600

    Theme and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# simple-bar-night-owl
+
+A theme for [simple-bar](https://github.com/Jean-Tinland/simple-bar) based on the [Night Owl theme](https://github.com/sdras/night-owl-vscode-theme) for Visual Studio Code
+
+## Installation
+
+See [this section](https://github.com/Jean-Tinland/simple-bar#colors--theme) of simple-bar's readme
---
+++ night-owl.js
@@ -0,0 +1,33 @@
+// Comments indicate where colors were taken from the source VSCode theme:
+// https://github.com/sdras/night-owl-vscode-theme/blob/main/themes/Night%20Owl-color-theme.json
+
+export const NightOwl = {
+	name: "Night Owl",
+	kind: "dark",
+	main: "#011627", // editor.background
+	mainAlt: "#9b57a2", // editor.background
+	minor: "#01111d", // tab.inactiveBackground
+	red: "#EF5350", // terminal.ansiRed
+	green: "#22da6e", // terminal.ansiGreen
+	yellow: "#ffeb95", // terminal.ansiBrightYellow
+	orange: "#f78c6c", // constant.numeric
+	blue: "#82AAFF", // terminal.ansiBlue
+	magenta: "#C792EA", // terminal.ansiMagenta
+	cyan: "#7fdbca", // terminal.ansiBrightCyan
+	black: "#011627", // terminal.ansiBlack
+	white: "#ffffff", // terminal.ansiWhite
+	foreground: "#d6deeb", // editor.foreground
+	transparentDark: "rgba(0, 0, 0, 0.05)",
+	defaultFont: "JetBrains Mono, monospace",
+	barHeight: "28px",
+	barRadius: "3px",
+	barInnerMargin: "3px",
+	itemRadius: "2px",
+	itemInnerMargin: "3px 7px",
+	itemOuterMargin: "0 0 0 5px",
+	hoverRing: "0 0 0 2px rgba(255, 255, 255, 0.75)",
+	focusRing: "0 0 0 2px rgb(255, 255, 255)",
+	lightShadow: "0 5px 10px rgba(0, 0, 0, 0.24)",
+	transitionEasing: "cubic-bezier(0.4, 0, 0.2, 1)",
+	clickEffect: "rgba(255, 255, 255, 0.3)",
+};
